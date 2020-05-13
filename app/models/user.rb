93bc0f016643898a18f9c1bed5c6373b73b9d60a
misakisu <che.v.jq@gmail.com>
2020-05-13 17:32:34 +0900

    First update

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image#refileを使って、画像投稿ができるよう,refile特有の記述としてuser.rbへ記入。
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'e7a10eec06fd19ab81487ebd7c018fb03d3fa24993c99d5bc253161148723b4801bb5b4237f49e79ebe4806d6b65d1b1d979dfc5f7a77952101fd936d6a04e2b'
